f7d697e637a5828af546a925dfc8e9342f344212
Michael Prokofyev <mikeprokofyev@gmail.com>
2018-07-02 14:12:16 +0300

    init

---
+++ Assets/Scripts/BodyPart.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyPart : MonoBehaviour {
+
+    public int excersiseFatigueCost = 10;
+
+    int fatigue = 0;
+
+    [SerializeField] private ObjectShake shaker;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+    }
+
+    void OnMouseDown()
+    {
+        fatigue += excersiseFatigueCost;
+        shaker.ShakeObject();
+        print("CLICKED");
+    }
+}
+
